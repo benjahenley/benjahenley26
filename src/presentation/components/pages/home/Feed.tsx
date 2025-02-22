8ab4d66756290a7517eb974b06e7ce2e89fdaf3e
benjahenley <benjahenley@hotmail.com>
2025-02-22 18:13:39 -0300

    fix issue with lint

--- src/presentation/components/pages/home/Feed.tsx
+++ src/presentation/components/pages/home/Feed.tsx
@@ -3,7 +3,6 @@
 import Tweet from "../../ui/interactions/comments/ProjectComment";
 import { Locales, TweetApiResponse } from "@/infraestructure/interfaces";
 import { Spinner } from "../../ui/Spinner";
-import { useQuery } from "@apollo/client";
 import { GET_TWEETS_BY_CATEGORY } from "@/graphql/queries/getTweets";
 import { TweetCategory } from "@/graphql/types";
 
@@ -13,39 +12,31 @@ type Props = {
 };
 
 export default function Feed({ locale, className }: Props) {
-  const { loading, error, data } = useQuery(GET_TWEETS_BY_CATEGORY, {
-    variables: {
-      language: locale as string,
-      category: TweetCategory.FEED,
-    },
-    errorPolicy: "all",
-  });
-
-  if (loading) {
-    return (
-      <>
-        <Spinner></Spinner>
-      </>
-    );
-  }
-  if (error) {
-    console.log("error", error);
-    return (
-      <div className="w-full h-auto flex justify-center items-center">
-        <img src="/photos/error/404.jpg" className="w-full h-auto "></img>
-      </div>
-    );
-  }
-
-  console.log({ data });
-
+  // if (loading) {
+  //   return (
+  //     <>
+  //       <Spinner></Spinner>
+  //     </>
+  //   );
+  // }
+  // if (error) {
+  //   console.log("error", error);
   return (
-    <div className={className}>
-      {data.getTweetsByCategory.map(
-        (tweet: TweetApiResponse, index: number) => (
-          <Tweet {...tweet} key={index}></Tweet>
-        )
-      )}
+    <div className="w-full h-auto flex justify-center items-center">
+      <img src="/photos/error/404.jpg" className="w-full h-auto "></img>
     </div>
   );
 }
+
+// console.log({ data });
+
+// return (
+//   <div className={className}>
+//     {data.getTweetsByCategory.map(
+//       (tweet: TweetApiResponse, index: number) => (
+//         <Tweet {...tweet} key={index}></Tweet>
+//       )
+//     )}
+//   </div>
+// );
+// }
