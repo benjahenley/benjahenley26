57a04d1793551807e70221bdabc6b67d272e4cfe
benjahenley <benjahenley@hotmail.com>
2025-03-02 17:34:49 -0300

    FIX - build fail in Vercel patched by removing yup validations

--- src/validations/commentSchema.ts
+++
@@ -1,6 +0,0 @@
-import * as yup from "yup";
-
-export const commentSchema = yup.object({
-  content: yup.string().required("Content is required"),
-  tweetId: yup.number().integer().required("Tweet ID is required"),
-});
--- src/validations/tweetIdSchema.ts
+++
@@ -1,9 +0,0 @@
-import * as yup from "yup";
-
-export const tweetIdSchema = yup.object({
-  id: yup
-    .number()
-    .required("Tweet ID is required")
-    .integer("Tweet ID must be an integer")
-    .positive("Tweet ID must be a positive number"),
-});
